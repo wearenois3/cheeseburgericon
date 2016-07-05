75297066cd25b67831017504ebb93fc8fcb5fb58
carlo frinolli <carlo.frinolli@nois3.it>
2016-07-05 12:08:07 +0200

    new server to deploy to

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,8 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'chocolatmilano.it', user: 'deploy', roles: %w{web app db}
+server '95.85.7.16', user: 'www-data', roles: %w{web app db}
+set :deploy_to, "/var/www/#{fetch(:application)}"
 
 
 # Custom SSH Options
